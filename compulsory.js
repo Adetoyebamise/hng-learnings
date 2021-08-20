578ebcbfb6f656f8abf200d5f2b0dde1a4a0dc6a
Adetoye Babalola <adetoyebamise@gmail.com>
2021-08-20 09:35:42 +0100

    add compulsory file

---
+++ compulsory.js
@@ -0,0 +1,7 @@
+const name = "adetoyebamise"
+const id = "unknown"
+const email = "adetoyebamise@gmail.com"
+const output = (name, id, email) => {
+  return `Hey there, this is ${name} with ID ${id} and email ${email}`
+}
+console.log(output(name, id, email))
